1c7877674dac366d445029a2416abb392913fc58
Fistandantilus <gregorymelancon@gmail.com>
2025-04-16 21:18:34 -0500

    B: Added circleshape.py

--- main.py
+++ main.py
@@ -4,12 +4,14 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
+clock = pygame.time.Clock()
+dt = 0 # Initiates timekeeping and creates variable to tickspeed
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}") # Displays startup message and screen variables to terminal
     running = True
     while running:
         for event in pygame.event.get():
@@ -17,6 +19,9 @@ def main():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000  # Convert milliseconds to seconds
+
 
 if __name__ == "__main__":
     main()
